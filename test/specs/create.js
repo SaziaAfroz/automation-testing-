75f1a26b1b6892b0466c1c3848852f51d5d3c9f1
DESKTOP-2NRHQ2J\User2 <sazia.afroz@selise.ch>
2024-01-16 13:29:04 +0600

    assignment-5 done

--- test/specs/create.js
+++ test/specs/create.js
@@ -1,6 +1,7 @@
 
 import LoginPage from '../pageobjects/login.page.js'
 import CreateProject from '../pageobjects/create.project.js'
+import generic from '../pageobjects/generic.js'
 
 
 
@@ -15,25 +16,20 @@ describe('My Login application', () => {
 
     it('Project Creation', async () => {
         await CreateProject.createProjectbutton.click()
-        await CreateProject.projectName.setValue('hululu')
+        await CreateProject.projectName.setValue('Automation test project')
         await CreateProject.projectNumber.setValue('1234')
-        await browser.pause(5000)
+        //await browser.pause(5000)
         await CreateProject.dropdownIcon.scrollIntoView()
         await CreateProject.dropdownIcon.parentElement().click();
         await CreateProject.selectCountry.click()
         //await CreateProject.dropdownIcon.click()
-        await browser.pause(10000)
+        //await browser.pause(10000)
 
 
 
         await CreateProject.btnSubmit.click()
         //await browser.pause(10000)
-        await browser.waitUntil(async function () {
-            return (await CreateProject.successToast).isExisting()
-        },{
-            timeout: 10000,
-
-        })
+        await generic.genericWaitUntil(await CreateProject.successToast)
         await expect(await CreateProject.successToast).toHaveTextContaining('Project created successfully')
 
 
